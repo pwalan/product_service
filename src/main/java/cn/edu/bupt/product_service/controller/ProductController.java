fcb26167370e7352b401f374f6e09ed21ff3fd93
alanp <pwalan@foxmail.com>
2018-12-31 09:03:42 +0800

    add port on controller

--- src/main/java/cn/edu/bupt/product_service/controller/ProductController.java
+++ src/main/java/cn/edu/bupt/product_service/controller/ProductController.java
@@ -1,7 +1,10 @@
 package cn.edu.bupt.product_service.controller;
 
+import cn.edu.bupt.product_service.domain.Product;
 import cn.edu.bupt.product_service.service.ProductService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,16 +13,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/product")
 public class ProductController {
 
+    @Value("${server.port}")
+    private String port;
+
     @Autowired
     private ProductService productService;
 
     @RequestMapping("list")
-    public Object list(){
+    public Object list() {
         return productService.listProduct();
     }
 
     @RequestMapping("find")
-    public Object findById(@RequestParam("id") int id){
-        return productService.findById(id);
+    public Object findById(@RequestParam("id") int id) {
+        Product product = productService.findById(id);
+        Product result = new Product();
+        BeanUtils.copyProperties(product, result);
+        result.setName(result.getName() + " data from " + port);
+        return result;
     }
 }
