479ab6605fa7b4c128b85d6cff80c3b27663f511
alanp <pwalan@foxmail.com>
2019-01-05 23:05:15 +0800

    use rabbitmq

--- src/main/java/cn/edu/bupt/product_service/controller/ProductController.java
+++ src/main/java/cn/edu/bupt/product_service/controller/ProductController.java
@@ -5,16 +5,21 @@ import cn.edu.bupt.product_service.service.ProductService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/product")
+@RefreshScope
 public class ProductController {
 
     @Value("${server.port}")
     private String port;
+
+    @Value("${env}")
+    private String env;
 
     @Autowired
     private ProductService productService;
@@ -29,7 +34,7 @@ public class ProductController {
         Product product = productService.findById(id);
         Product result = new Product();
         BeanUtils.copyProperties(product, result);
-        result.setName(result.getName() + " data from " + port);
+        result.setName(result.getName() + " data from " + port + " " + env);
         return result;
     }
 }
